d39a042f2ffa6fc4ac5e4e3702d6b393df0ad623
Victor Teka <ambukateka@gmail.com>
2021-03-10 02:57:51 +0300

    Create typeorm config

---
+++ ormconfig.js
@@ -0,0 +1,24 @@
+const config = require('dotenv').config
+
+config()
+
+module.exports = {
+    type: process.env.DB_TYPE,
+    host: process.env.HOST,
+    database: process.env.DB,
+    port: process.env.DB_PORT,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    entities: ["dist/models/*.js"],
+    migrations: ["dist/migration/*.js"],
+    logging: true,
+    synchronize: false,
+    migrationsRun: true,
+    cli: {
+      migrationsDir: "src/migration",
+      entitiesDir: "src/models"
+    },
+    extra: {
+      connectionLimit: 20
+    }
+}
